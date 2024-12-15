2e28a854231532e5dbb1bbc8bad3291cf15dd4ed
Armstrong <capkrypton@gmail.com>
2024-12-15 17:44:14 +0200

    change supar base url and key

--- src/index.js
+++ src/index.js
@@ -7,8 +7,8 @@ import { createClient } from '@supabase/supabase-js';
 import { SessionContextProvider } from '@supabase/auth-helpers-react';
 
 const supabase = createClient(
-  "https://kutohnpanfgfzvfyoqph.supabase.co",
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imt1dG9obnBhbmZnZnp2ZnlvcXBoIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzQwODQxMzMsImV4cCI6MjA0OTY2MDEzM30.s0AUMkn9ufAyqY_q7T9vc1Cl6KwyLA1kecCmodorARs" 
+  "https://hlmddpjiwdbuefrdknoc.supabase.co",
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImhsbWRkcGppd2RidWVmcmRrbm9jIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzQyNzY1MzQsImV4cCI6MjA0OTg1MjUzNH0._Q3va55vHmAUi0r5yiK-wkPzlCHcFwtFzIn2JzIrd_A" 
 );
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
